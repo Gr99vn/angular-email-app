463f6e63a957852759c70f61a9ea553c679de02b
Gr99vn <duc.trantrung99@gmail.com>
2021-11-17 21:25:24 +0700

    test

--- src/app/messages/email-detail/email-detail.component.ts
+++ src/app/messages/email-detail/email-detail.component.ts
@@ -4,18 +4,18 @@ import { IMessage } from 'src/app/messages.service';
 @Component({
   selector: 'app-email-detail',
   templateUrl: './email-detail.component.html',
-  styleUrls: ['./email-detail.component.scss']
+  styleUrls: ['./email-detail.component.scss'],
 })
 export class EmailDetailComponent implements OnInit, OnChanges {
+  @Input() message?: IMessage;
+  messageBodyAsHTML: string = '';
+  constructor() {}
 
- @Input() message?: IMessage;
- messageBodyAsHTML: string = '';
-  constructor() { }
-
-  ngOnInit(): void {
-    
-  }
+  ngOnInit(): void {}
   ngOnChanges() {
-    this.messageBodyAsHTML = this.message?.body.split(/\n/).map(p => `<p>${p}</p>`).join('\n')!;
+    this.messageBodyAsHTML = this.message?.body
+      .split(/\n/)
+      .map((p) => `<p>${p}</p>`)
+      .join('\n')!;
   }
 }
